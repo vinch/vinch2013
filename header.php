cc28d878dc18db26b3eba8a0f2e109a39a9e0297
vinch <vincent.battaglia@gmail.com>
2013-12-03 00:28:40 -0800

    Changed title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
-	<title><?php wp_title(); ?></title>
+	<title><?php wp_title('/', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 	<link rel="alternate" type="application/rss+xml" title="Le blog de Vincent Battaglia" href="http://feeds.feedburner.com/vinch" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -12,9 +12,9 @@
 	<![endif]-->
   <script type="text/javascript" src="http://use.typekit.com/pek2yuw.js"></script>
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <meta name="google-translate-customization" content="d2cb1b370495bdd2-939990c76313e922-gef469feeccf0a079-c" />
 	<meta name="google-site-verification" content="fu4OZZhZ-OTLDfSe2syR1Y620bAiXFYhfNlO0XQh3SE" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<meta name="google-translate-customization" content="d2cb1b370495bdd2-939990c76313e922-gef469feeccf0a079-c" />
 </head>
 <body <?php body_class(); ?>>
 
